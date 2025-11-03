19370d307c4d92e4b4c9a5d9d70c4af758a1ff77
redseiko <redseiko@gmail.com>
2025-11-02 21:29:55 -0800

    Create YoloCapsuleController and all sorts of fun Rigidbody related shenanigans.

---
+++ GameOff2025UnityProject/Assets/Scripts/GameJam/Game/YoloCapsuleController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace GameJam {
+  public sealed class YoloCapsuleController : MonoBehaviour {
+    [field: SerializeField]
+    public float CapsuleMass { get; set; } = 80f;
+
+    [field: SerializeField]
+    public float CapsulePushForce { get; set; } = 0.1f;
+
+    void OnControllerColliderHit(ControllerColliderHit hit) {
+      if (hit.collider.transform.root == transform.root) {
+        return;
+      }
+
+      Rigidbody rigidbody = hit.collider.attachedRigidbody;
+
+      if (!rigidbody || rigidbody.isKinematic) {
+        return;
+      }
+
+      Vector3 pushDirection = new Vector3(hit.moveDirection.x, 0f, hit.moveDirection.z).normalized;
+
+      float massRatio = CapsuleMass / rigidbody.mass;
+      massRatio = Mathf.Clamp(massRatio, 0.1f, 10f);
+
+      rigidbody.AddForce(CapsulePushForce * massRatio * pushDirection, ForceMode.Impulse);
+    }
+  }
+}
