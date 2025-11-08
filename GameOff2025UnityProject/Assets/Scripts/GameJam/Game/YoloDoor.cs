1171d62d5efac67213f742b6f163e80a1361bc0d
redseiko <redseiko@users.noreply.github.com>
2025-11-07 21:06:43 -0800

    Adjusted `YoloDoor` to disable colliders when opening/closing.

--- GameOff2025UnityProject/Assets/Scripts/GameJam/Game/YoloDoor.cs
+++ GameOff2025UnityProject/Assets/Scripts/GameJam/Game/YoloDoor.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using DG.Tweening;
 
 using UnityEngine;
@@ -7,6 +9,9 @@ namespace GameJam {
     [field: SerializeField]
     public Transform DoorHinge { get; private set; }
 
+    [field: SerializeField]
+    public List<Collider> DoorColliders { get; private set; } = new();
+
     [field: Header("OpenDoor")]
     [field: SerializeField]
     public Vector3 OpenDoorRotation { get; set; } = new(0f, 90f, 0f);
@@ -15,11 +20,34 @@ namespace GameJam {
     public float OpenDoorDuration { get; set; } = 0.5f;
 
     [field: Header("State")]
+    [field: SerializeField]
     public bool IsDoorOpen { get; private set; } = false;
 
+    Sequence _openDoorSequence = default;
+
     public void OpenDoor() {
-      DoorHinge.DOBlendableRotateBy(IsDoorOpen ? OpenDoorRotation * -1f : OpenDoorRotation, OpenDoorDuration);
-      IsDoorOpen = !IsDoorOpen;
+      if (_openDoorSequence.IsActive() && _openDoorSequence.IsComplete()) {
+        return;
+      }
+
+      ToggleDoorColliders(toggleOn: false);
+
+      Vector3 rotation = IsDoorOpen ? OpenDoorRotation * -1f : OpenDoorRotation;
+      bool isDoorOpen = !IsDoorOpen;
+
+      _openDoorSequence = DOTween.Sequence()
+          .Append(DoorHinge.DOBlendableLocalRotateBy(rotation, OpenDoorDuration))
+          .OnComplete(
+              () => {
+                ToggleDoorColliders(toggleOn: true);
+                IsDoorOpen = isDoorOpen;
+              });
+    }
+
+    void ToggleDoorColliders(bool toggleOn) {
+      foreach (Collider collider in DoorColliders) {
+        collider.enabled = toggleOn;
+      }
     }
   }
 }
