6aee8e484536d9a027fb36a36648b1ab4c3f688d
iesuzume <csth.mem@gmail.com>
2025-11-09 14:41:21 -0800

    Added OnExit and OnStay events to CollideInteractable.

--- GameOff2025UnityProject/Assets/Scripts/GameJam/Game/CollideInteractable.cs
+++ GameOff2025UnityProject/Assets/Scripts/GameJam/Game/CollideInteractable.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace GameJam
-{
-    public class CollideInteractable : MonoBehaviour
-    {
+namespace GameJam {
+  public class CollideInteractable : MonoBehaviour {
     [field: Header("Events"), Space(10f)]
     [field: SerializeField]
     public UnityEvent<GameObject> OnCollide { get; private set; }
 
+    [field: SerializeField]
+    public UnityEvent<GameObject> OnStay { get; private set; }
+
+    [field: SerializeField]
+    public UnityEvent<GameObject> OnExit { get; private set; }
+
     void OnTriggerEnter(Collider collider) {
       if (collider.TryGetComponent(out CharacterController _)) {
-        OnCollide.Invoke(this.gameObject);
+        OnCollide?.Invoke(this.gameObject);
+      }
+    }
+
+    private void OnTriggerStay(Collider collider) {
+      if (collider.TryGetComponent(out CharacterController _)) {
+        OnStay?.Invoke(this.gameObject);
+      }
+    }
+
+    private void OnTriggerExit(Collider collider) {
+      if (collider.TryGetComponent(out CharacterController _)) {
+        OnExit?.Invoke(this.gameObject);
       }
     }
   }
