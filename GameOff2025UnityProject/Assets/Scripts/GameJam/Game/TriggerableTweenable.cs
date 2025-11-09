45958e917268931ced78ddc8c86d675a24600b7b
iesuzume <csth.mem@gmail.com>
2025-11-08 23:02:40 -0800

    Added a test wheel for testing TriggerableTweenable

--- GameOff2025UnityProject/Assets/Scripts/GameJam/Game/TriggerableTweenable.cs
+++ GameOff2025UnityProject/Assets/Scripts/GameJam/Game/TriggerableTweenable.cs
@@ -4,6 +4,8 @@ using UnityEditor.Experimental.GraphView;
 
 using UnityEngine;
 
+using YoloBox;
+
 namespace GameJam {
   public class TriggerableTweenable : MonoBehaviour {
     [field: SerializeField]
@@ -15,11 +17,14 @@ namespace GameJam {
     [field: SerializeField]
     public Vector3 ScaleBy { get; private set; } = Vector3.zero;
 
+    [field: SerializeField]
+    public bool IsContinuous { get; private set; } = false;
+
     Sequence _tweenSequence = default;
 
     public void TriggerTween(float duration) {
       if (_tweenSequence.IsActive()) {
-        if (_tweenSequence.IsComplete()) {
+        if (!IsContinuous && _tweenSequence.IsComplete()) {
           return;
         } else if (!_tweenSequence.IsPlaying()) {
           _tweenSequence.PlayForward();
@@ -27,18 +32,27 @@ namespace GameJam {
       } else {
         _tweenSequence = DOTween.Sequence()
           .Append(this.gameObject.transform.DOBlendableLocalMoveBy(TranslateBy, duration))
-          .Join(this.gameObject.transform.DOBlendableLocalRotateBy(RotateBy, duration))
+          .Join(this.gameObject.transform.DOBlendableLocalRotateBy(RotateBy, duration).SetEase(Ease.Linear))
           .Join(this.gameObject.transform.DOBlendableScaleBy(ScaleBy, duration))
+          .SetAutoKill(false)
           .SetUpdate(UpdateType.Fixed);
+
+        if (IsContinuous) {
+          _tweenSequence.AppendCallback(this.RepeatTween);
+        }
       }
     }
 
     public void PauseTween() {
-      if (!_tweenSequence.IsActive() || _tweenSequence.IsComplete()) {
+      if (!_tweenSequence.IsActive() || (!IsContinuous && _tweenSequence.IsComplete())) {
         return;
       }
 
       _tweenSequence.Pause();
+    }
+
+    private void RepeatTween() {
+      _tweenSequence.PlayAgain();
     }
   }
 }
